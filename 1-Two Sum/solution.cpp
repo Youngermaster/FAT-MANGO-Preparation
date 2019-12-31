e44ac9fe12e935e34a798db4a4b156c06a40f266
Youngermaster <jmyoungh@eafit.edu.co>
2019-12-31 12:42:14 -0500

    Second commit

---
+++ 1-Two Sum/solution.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+#include "vector"
+
+int main(int argc, char const *argv[])
+{
+    std::cout << twoSum() << std::endl;
+    return 0;
+}
+
+std::vector<int> twoSum() {
+    int n = 1;
+    std::vector<int> f = std::vector{1, 2};
+    return f;
+}
--- 1-Two Sum/solution.js
+++ 1-Two Sum/solution.js
@@ -11,5 +11,6 @@ function twoSum(nums, target) {
         }
     }
 }
-
-console.log('Solution:', twoSum([2, 7, 11, 15], 9));
+// array = [2, 7, 11, 15]
+array = [2, 11, 15, 7]
+console.log('Solution:', twoSum(array, 9));
