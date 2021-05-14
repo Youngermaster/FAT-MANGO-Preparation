e6f87973207cd02bcde1bf79b0a04db106a98bae
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-05-13 22:17:08 -0500

    A.Team problem added

---
+++ CodeForces/A.Team.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, Petya, Vasya, Tonya, problemsToSolve(0);
+    cin >> n;
+    while (n--)
+    {
+        cin >> Petya >> Vasya >> Tonya;
+        if (Petya + Vasya + Tonya >= 2)
+            problemsToSolve += 1;
+    }
+    cout << problemsToSolve << endl;
+    return 0;
+}
