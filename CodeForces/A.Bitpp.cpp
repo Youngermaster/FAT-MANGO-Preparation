811c88f587524726fb2c25e8455d0287ee5424bd
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-05-14 22:08:03 -0500

    Performance option test

--- CodeForces/A.Bitpp.cpp
+++ CodeForces/A.Bitpp.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int main()
 {
     int n, x(0);
+    // Just a test if the performance would change, but with so little data it doesn't change
+    ios_base::sync_with_stdio(false); 
+
     cin >> n;
 
     string s;
