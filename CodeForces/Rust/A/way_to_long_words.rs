ddee86c029e27bfda142564591aafa28235d0b39
Youngermaster <juanmanuel12.13jmyh81@gmail.com>
2021-06-07 22:48:31 -0500

    Not working Yet

---
+++ CodeForces/Rust/A/way_to_long_words.rs
@@ -0,0 +1,25 @@
+
+fn get_input() -> String {
+    let mut buffer = String::new();
+    std::io::stdin().read_line(&mut buffer).expect("Failed");
+    buffer
+}
+
+fn main() {
+    let n = get_input().trim().parse::<i64>().unwrap();
+    for _i in 0..n {
+        let word = get_input();
+        if word.len() > 10 {
+            let mut b: i32 = (word.len() as i32) - 2;
+            b -= 2;
+            println!(
+                "{}{}{}",
+                &word[0..1],
+                b,
+                word.trim().chars().last().unwrap()
+            );
+        } else {
+            println!("{}", word.trim());
+        }
+    }
+}
