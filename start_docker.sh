3f77bc6416e7b28456fd3f3baed49ec9bc287c84
Juan Manuel Young Hoyos <vndrzrfshJXY@wegmans.com>
2024-04-26 03:35:58 -0500

    📦 Refactor Dockerfile and start_docker.sh for improved containerization

--- start_docker.sh
+++ start_docker.sh
@@ -1,2 +1,2 @@
 docker build -t fat-mango-compiler .
-docker run -it --name fat-mango-compiler -v $(pwd):/usr/src/app fat-mango-compiler
+docker run -it --rm --name fat-mango-compiler -v $(pwd):/usr/src/app fat-mango-compiler
